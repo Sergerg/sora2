2f166b37571e775e7b93170bf60bc6d7536b72fd
Serger <sergerg@gmal.com>
2016-08-27 21:55:21 +0400

    ScreenBean

--- src/main/java/org/sora/fx/Application.java
+++ src/main/java/org/sora/fx/Application.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.sora.fx.beans.MainScreenBean;
+import org.sora.fx.beans.ScreenBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -35,7 +36,7 @@ public class Application extends javafx.application.Application {
         log.debug("Spring context load ok.");
 
         // start JavaFX spring configuration
-        MainScreenBean mainScreenBean = context.getBean("mainScreenBean", MainScreenBean.class);
+        ScreenBean mainScreenBean = context.getBean("mainScreenBean", ScreenBean.class);
         mainScreenBean.setStage(primaryStage);
         mainScreenBean.show();
 
